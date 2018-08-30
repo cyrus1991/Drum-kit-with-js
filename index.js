a532017b91e62a074066dd86131e51d1fd8d1f4a
CYRUS <cyrussmith2017@gmail.com>
2018-08-31 01:13:23 +0200

    the audio works now

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hi")
+
+
+// so here we say whenever i click on the key that function 
+// should get called (run)
+// nad the e here is an obj full of data
+
+window.addEventListener('keydown',function (e) {
+    // is there any element on the page that has a data-key of 
+    // the keycode that we press?
+    // so because I am just looking for one element use 
+   // querySelector if I was looking for many I would use querySelectorAll
+const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+if (!audio) return // stop func from runnig all to gether
+audio.play();
+    console.log(audio)
+});
+
+
