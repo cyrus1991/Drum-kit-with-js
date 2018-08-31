ff6f1941b4ffed6c47ff388c2ec342d134fb83ec
CYRUS <cyrussmith2017@gmail.com>
2018-08-31 02:29:55 +0200

    now you can play your piano with the keyboard

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-
-
-// so here we say whenever i click on the key that function 
-// should get called (run)
-// nad the e here is an obj full of data
-
-window.addEventListener('keydown',function (e) {
+// e here is an obj full of data
+function playSound (e) {
     // is there any element on the page that has a data-key of 
     // the keycode that we press?
     // so because I am just looking for one element use 
-   // querySelector if I was looking for many I would use querySelectorAll
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-if (!audio) return // stop func from runnig all to gether
-audio.currentTime=0; // rewind to the start 
-audio.play();
-    console.log(key)
-});
+    // querySelector if I was looking for many I would use querySelectorAll
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if (!audio) return // stop func from runnig all to gether
+    audio.currentTime=0; // rewind to the start 
+    audio.play();
+    key.classList.add('playing'); // its exactly like key.addclass('playig)
+    // so that makes a new class to the element
+};
+function removeTransition (e) {
+    if(e.propertyName !== 'transform')  return // skip it if its not a transform
+    this.classList.remove('playing')
+};
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+// so here I say whenever I click on the key playSound function 
+// should get called (run)
+window.addEventListener('keydown', playSound)
+   
+
 
 
