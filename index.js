d2229dce782ce25924168510cba6d990a7321efc
cyrus.sarwale <cyrus.sarwale@misterspex.de>
2019-11-12 12:47:21 +0100

    style: clean code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // e here is an obj full of data
 function playSound (e) {
-    // is there any element on the page that has a data-key of 
+    // is there any element on the page that has a data-key of
     // the keycode that we press?
-    // so because I am just looking for one element use 
+    // so because I am just looking for one element use
     // querySelector if I was looking for many I would use querySelectorAll
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return // stop func from runnig all to gether
-    audio.currentTime=0; // rewind to the start 
+    audio.currentTime=0; // rewind to the start
     audio.play();
     key.classList.add('playing'); // its exactly like key.addclass('playig)
     // so that makes a new class to the element
@@ -18,10 +18,6 @@ function removeTransition (e) {
 };
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend',removeTransition));
-// so here I say whenever I click on the key playSound function 
+// so here I say whenever I click on the key playSound function
 // should get called (run)
 window.addEventListener('keydown', playSound)
-   
-
-
-
